f5d98e7a62788725a0349cb3aceed3a223d355ad
Ng Kok Wah <kokwah98@gmail.com>
2017-09-06 19:55:22 +0800

    Add files via upload

---
+++ RandomNumberGenerator/GenerateNumber.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace RandomNumberGenerator
+{
+    partial class GenerateNumber
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.generate = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(94, 69);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(282, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Range";
+            // 
+            // generate
+            // 
+            this.generate.Location = new System.Drawing.Point(158, 191);
+            this.generate.Name = "generate";
+            this.generate.Size = new System.Drawing.Size(146, 53);
+            this.generate.TabIndex = 2;
+            this.generate.Text = "Generate";
+            this.generate.UseVisualStyleBackColor = true;
+            this.generate.Click += new System.EventHandler(this.generate_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(94, 345);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(282, 22);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 348);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Number";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(105, 103);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "e.g. 0-100";
+            // 
+            // GenerateNumber
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(436, 496);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.generate);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "GenerateNumber";
+            this.Text = "Generate Random Number";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button generate;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ RandomNumberGenerator/GenerateNumber.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RandomNumberGenerator
+{
+    public partial class GenerateNumber : Form
+    {
+        private static int startnum;
+        private static int endnum;
+        public GenerateNumber()
+        {
+            InitializeComponent();
+        }
+
+        private void generate_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Equals(null) || textBox1.Equals(""))
+            {
+                MessageBox.Show("Please enter a range to continue!");
+            }
+            else
+            {
+                Random ran = new Random();
+                if (textBox1.Text.Contains("-"))
+                {
+                    String[] numbers = textBox1.Text.Split('-');
+                    if (numbers[0].Contains(" ") || numbers[1].Contains(" "))
+                    {
+                        numbers[0].Trim(' ');
+                        numbers[1].Trim(' ');
+                        startnum = Convert.ToInt32(numbers[0]);
+                        endnum = Convert.ToInt32(numbers[1]);
+                        textBox2.Text = Convert.ToString(ran.Next(startnum, endnum));
+                    }
+                    else
+                    {
+                        startnum = Convert.ToInt32(numbers[0]);
+                        endnum = Convert.ToInt32(numbers[1]);
+                        textBox2.Text = Convert.ToString(ran.Next(startnum, endnum));
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Please enter in the specified format! e.g. 0-100");
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
